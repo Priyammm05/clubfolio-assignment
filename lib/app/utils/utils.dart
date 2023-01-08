99d89b7236ca490e6cec279e025882c48961166c
Priyam <priyam05soni@gmail.com>
2023-01-08 19:12:34 +0530

    utils

---
+++ lib/app/utils/utils.dart
@@ -0,0 +1,8 @@
+// COLORS
+import 'package:hexcolor/hexcolor.dart';
+
+var yellowColor = HexColor("#F7C97E");
+var greyColor = HexColor("#191919");
+var darkGreyColor = HexColor("#222222");
+var lightColor = HexColor("#FFFFFF").withOpacity(0.5);
+var backgroundColor = HexColor("#1E1E1E");
